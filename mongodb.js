c0ce76299b403b6f4331c77161f72525d4211ce0
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-06-26 23:51:04 +0530

    mongodb.js

---
+++ mongodb.js
@@ -0,0 +1,11 @@
+//CRUD Create Read Update Delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL,{ useNewURLParser: true})
+
