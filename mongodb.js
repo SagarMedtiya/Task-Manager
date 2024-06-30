cd025c3f06f2433548f5d5eb29fa057575278c7c
Sagar Medtiya <sagarmedtiya@gmail.com>
2024-06-30 23:59:22 +0530

    mopgnodb

--- mongodb.js
+++ mongodb.js
@@ -1,16 +1,20 @@
 //CRUD Create Read Update Delete
 
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const client = new MongoClient(connectionURL,{monitorCommands:true});
+const id = new ObjectId()
 
 const databaseName = 'task-manager'
 
 client.connect();
 const db = client.db(databaseName)
-db.collection('users').insertOne({
-    name: 'Sagar',
-    age: 25
-})  
+db.collection('users').findOne({ name: 'Sagar'},(error, user)=>{
+    if(error){
+        return console.log('Unable to fetch')
+    }
+
+    console.log(user)
+})
 
